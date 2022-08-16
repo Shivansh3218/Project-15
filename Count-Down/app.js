684afd93e11214578ae3d6c36b456546d95f2a4c
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-08-16 10:23:59 +0530

    Count-Down

---
+++ Count-Down/app.js
@@ -0,0 +1,92 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const giveaway = document.querySelector('.giveaway');
+  const deadline = document.querySelector('.deadline');
+  const items = document.querySelectorAll('.deadline-format h4');
+  
+  let tempDate = new Date();
+  let tempYear = tempDate.getFullYear();
+//   console.log(tempYear)
+  let tempMonth = tempDate.getMonth();
+  let tempDay = tempDate.getDate();
+  // months are ZERO index based;
+  const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+  
+  
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+  
+  let month = futureDate.getMonth();
+  month = months[month];
+  const weekday = weekdays[futureDate.getDay()];
+  const date = futureDate.getDate();
+  giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+  
+  const futureTime = futureDate.getTime();
+  function getRemaindingTime() {
+    const today = new Date().getTime();
+  
+    //This t represents the time difference in ms
+
+    const t = futureTime - today;
+    // 1s = 1000ms
+    // 1m = 60s
+    // 1hr = 60m
+    // 1d = 24hr
+    // values in miliseconds
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    // calculate all values
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+  
+    // set values array
+    const values = [days, hours, minutes, seconds];
+
+    //If time is less than 10 then add 0 before it
+    function format(item) {
+      if (item < 10) {
+        return (item = `0${item}`);
+      }
+      return item;
+    }
+  //For each selects each item  to attach on the count down timer
+    items.forEach(function (item, index) {
+      item.innerHTML = format(values[index]);
+    });
+  //if the time came to an end
+    if (t < 0) {
+      clearInterval(countdown);
+      deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    }
+  }
+  // countdown clock added;
+  let countdown = setInterval(getRemaindingTime, 1000);
+  //set initial values
+  getRemaindingTime();
